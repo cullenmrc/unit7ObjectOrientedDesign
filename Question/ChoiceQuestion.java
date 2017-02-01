2548e5f34a8fc9d07c877770e8c9714dbef38893
Geoff Schmit <gcschmit@gmail.com>
2017-02-01 13:02:28 -0600

    refactored Question and ChoiceQuestion classes

--- Question/ChoiceQuestion.java
+++ Question/ChoiceQuestion.java
@@ -1,46 +1,64 @@
 import java.util.ArrayList;
 
 /**
-   A question with multiple choices.
-*/
+A question with multiple choices.
+ */
 public class ChoiceQuestion extends Question
 {
-   private ArrayList<String> choices;
+    private int numberOfChoices;
 
-   /**
-      Constructs a choice question with no choices.
-   */
-   public ChoiceQuestion()
-   {
-      choices = new ArrayList<String>();
-   }
+    /**
+    Constructs a choice question with no choices.
+     */
+    public ChoiceQuestion()
+    {
+        /*
+         * Java will automatically call the Question class's default
+         *      constructor before executing the next line of code.
+         */
+        numberOfChoices = 0;
+    }
+    
+    /**
+     * Constructs a choice question with the specified question text
+     */
+    public ChoiceQuestion( String questionText )
+    {
+        /*
+         * Explicitly call the Question class's constructor that takes
+         *      a questionText parameter. Calling a superclass's constructor
+         *      must be the first line of code in the subclass's constructor
+         */
+        super( questionText );
+        numberOfChoices = 0;
+    }
 
-   /**
-      Adds an answer choice to this question.
-      @param choice the choice to add
-      @param correct true if this is the correct choice, false otherwise
-   */
-   public void addChoice(String choice, boolean correct)
-   {
-      choices.add(choice);
-      if (correct) 
-      {
-         // Convert choices.size() to string
-         String choiceString = "" + choices.size();
-         setAnswer(choiceString);
-      }
-   }
-   
-   public void display()
-   {
-      // Display the question text
-      super.display();
-      // Display the answer choices
-      for (int i = 0; i < choices.size(); i++)
-      {
-         int choiceNumber = i + 1;
-         System.out.println(choiceNumber + ": " + choices.get(i));     
-      }
-   }
+    /**
+    Adds an answer choice to this question.
+    @param choice the choice to add
+    @param correct true if this is the correct choice, false otherwise
+     */
+    public void addChoice(String choice, boolean correct)
+    {
+        numberOfChoices++;
+        addText( "\n" + numberOfChoices + ")" + choice );
+        if (correct) 
+        {
+            String choiceString = "" + numberOfChoices;
+            setAnswer(choiceString);
+        }
+    }
+
+    public String toString()
+    {
+        /*
+         *  The Question class's implementation of the toString method returns
+         *      a string containing the text and answer. Call the superclass's
+         *      toString method first and then concatenate this class's
+         *      instance variables.
+         */
+        String s = super.toString();
+        return s + " numberOfChoices [" + numberOfChoices + "]";
+    }
 }
 
