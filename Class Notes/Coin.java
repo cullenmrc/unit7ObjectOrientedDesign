ec5b450d7618ff1592d974405a668e9212e34068
Geoff Schmit <gcschmit@gmail.com>
2017-02-06 13:18:43 -0600

    Coin now implements the Comparable interface.

--- Class Notes/Coin.java
+++ Class Notes/Coin.java
@@ -1,37 +1,91 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 /**
-   A coin with a monetary value.
-*/
-public class Coin
+A coin with a monetary value. Implements the Comparable interface.
+ */
+public class Coin implements Comparable<Coin>
 {
-   private double value;
-   private String name;
+    private double value;
+    private String name;
 
-   /**
-      Constructs a coin.
-      @param aValue the monetary value of the coin
-      @param aName the name of the coin
-   */
-   public Coin(double aValue, String aName) 
-   { 
-      value = aValue; 
-      name = aName;
-   }
+    /**
+    Constructs a coin.
+    @param aValue the monetary value of the coin
+    @param aName the name of the coin
+     */
+    public Coin(double aValue, String aName) 
+    { 
+        value = aValue; 
+        name = aName;
+    }
 
-   /**
-      Gets the coin value.
-      @return the value
-   */
-   public double getValue() 
-   {
-      return value;
-   }
+    /**
+    Gets the coin value.
+    @return the value
+     */
+    public double getValue() 
+    {
+        return value;
+    }
 
-   /**
-      Gets the coin name.
-      @return the name
-   */
-   public String getName() 
-   {
-      return name;
-   }
+    /**
+    Gets the coin name.
+    @return the name
+     */
+    public String getName() 
+    {
+        return name;
+    }
+    
+    public String toString()
+    {
+        return "value: " + this.value + "; name: " + this.name;
+    }
+
+    /**
+     * Compares two Coin objects.
+     * 
+     * @param otherObject the object to be compared
+     * @return a negative integer, zero, or a positive integer as this coin
+     *       is less than, equal to, or greater than the specified coin
+     */
+    public int compareTo( Coin other )
+    {
+        if( this.value < other.value )
+        {
+            return -1;
+        }
+        else if( this.value > other.value )
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    
+    public static void main( String[] args )
+    {
+        Coin nickel = new Coin( 0.05, "nickel" );
+        Coin dime = new Coin( 0.10, "dime" );
+        Coin quarter = new Coin( 0.25, "quarter" );
+        
+        ArrayList<Coin> list = new ArrayList<Coin>();
+        
+        list.add( dime );
+        list.add( quarter );
+        list.add( nickel );
+        
+        System.out.println( list );
+        
+        Collections.sort( list );
+        
+        System.out.println( list );
+    }
 }
+
+
+
+
